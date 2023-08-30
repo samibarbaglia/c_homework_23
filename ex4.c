4f8ddd82f83b972c8ff8a29530cb63845784edc7
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-08-30 17:41:52 +0300

    ex4

--- ex4.c
+++ ex4.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 int read_integer(void) {
-    float i = 0;
-    float numbers = 0;
-    float avg;
-    float sum = 0;
-    while (numbers >= 0) {
-        printf("Enter positive numbers or negative to stop: \n");
-        scanf("%f", &numbers);
-        if (numbers > 0) {
-            sum = sum + numbers;
-            i++;
-        }
-        else {
-            numbers = -1;
-        }
-    }
-    avg = sum / i;
-    printf("You entered %.0f positive numbers. The average is %.3f", i, avg);
+    int numbers;
+    scanf("%d", &numbers);
+    return numbers;
 }
 
 int main() {
-    read_integer();
+    int i = 0;
+    float avg;
+    float sum = 0;
+
+    while (i >= 0) {
+        printf("\nEnter positive numbers or negative to stop: ");
+        int numbers = read_integer();
+        if (numbers < 0) {
+            break;
+        }
+        sum = sum + numbers;
+        i++;
+    }
+    avg = sum / i;
+    printf("\nYou entered %.0d positive numbers. The average is %.3f", i, avg);
+
     return 0;
 }
