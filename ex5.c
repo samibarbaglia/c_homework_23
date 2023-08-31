242b983b1caed383b6f3c09f5d8af808cfaa1e9e
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-08-31 19:35:25 +0300

    ex5

--- ex5.c
+++ ex5.c
@@ -1,6 +1,39 @@
+// read more into pointers
 #include <stdio.h>
 
-int main(){
+int read_range(int low, int high) {
+    int player_num = low - 1;
 
+    do {
+        printf("\nEnter a number between %d and %d:", low, high);
+        scanf("%d", &player_num);
+        if (getchar() != '\n' || player_num > high || player_num < low) {
+            printf("!! Invalid input !!\n");
+        } else {
+            return player_num;
+        }
+    } while (1);
+}
+
+int main() {
+    int i = 0;
+    int low = 1;
+    int high = 6;
+
+    printf("Let's play!\nRoll a dice and enter your result.\n");
+
+    while (i < 3) {
+        int player_num = read_range(low, high);
+        int game_num = player_num + 1;
+
+        if (player_num < high) {
+            printf("I got %d. I win!\n", game_num);
+            i++;
+        } else {
+            printf("I got %d. It is a tie!\n", high);
+            i++;
+        }
+    }
+    printf("\nBetter luck next time. Bye!\n");
     return 0;
 }
