c3b5cb9e9a5298d8d348ae1e7e89457a36be07a4
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-03 22:17:22 +0300

    updated

--- ex3.c
+++ ex3.c
@@ -10,44 +10,40 @@ int main() {
     scanf("%d", &all_students);
 
     int student_grades[all_students];
-    for (i = 0; i < all_students; i++) {
+    for (i = 0; i <= all_students; i++) {
         student_grades[i] = -1;
     }
 
-    for (i = 0; i < all_students; i++) {
-
+    do {
         printf("Enter student number (1 - %d) or 0 to stop: \n", all_students);
         scanf("%d", &student);
 
         if (student == 0) {
             break;
-        }
-        else if (student > all_students) {
+        } else if (student > all_students) {
             printf("INVALID NUMBER\n");
-        }
-        else {
+        } else {
             do {
                 printf("Enter grade (0 - 5) for student %d or -1 to cancel: \n", student);
                 scanf("%d", &grade);
 
                 if (grade < -1 || grade > 5) {
                     printf("INVALID GRADE\n");
-                }
-                else {
+                } else {
                     student_grades[student] = grade;
                 }
             } while (grade < -1 || grade > 5);
         }
-    }
-    printf("\n%-10s %-10s\n", "STUDENT", "GRADE");
+    } while (student != 0);
 
-    for (i = 0; i < all_students; i++) {
-        if (student_grades[i] == -1) {
-            printf("%-10d %-10s\n", i + 1, "N/A");
+        printf("\n%-10s %-10s\n", "STUDENT", "GRADE");
+
+        for (i = 1; i <= all_students; i++) {
+            if (student_grades[i] == -1) {
+                printf("%-10d %-10s\n", i, "N/A");
+            } else {
+                printf("%-10d %-10d\n", i, student_grades[i]);
+            }
         }
-        else {
-            printf("%-10d %-10d\n", i + 1, student_grades[i]);
-        }
+        return 0;
     }
-    return 0;
-}
