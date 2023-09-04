af19998afa44da42f8521e301edd7e4fb7271671
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-04 14:06:24 +0300

    ex6

---
+++ ex6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int roll_dice(int dice) {
+    dice = ((rand() % dice) + 1);
+    printf("%d \n", dice);
+    return dice;
+}
+
+int dice_choice(int player_choice) {
+    if (player_choice == 1) {
+        roll_dice(6);
+    } else if (player_choice == 2) {
+        roll_dice(10);
+    }
+    return 0;
+}
+
+int main() {
+    int player_choice = 0;
+
+    do {
+        printf("\n1) Roll D6\n"
+               "2) Roll D10\n"
+               "3) Quit\n");
+        scanf("%d", &player_choice);
+
+        if (player_choice == 3) {
+            break;
+        } else if (player_choice != 1 && player_choice != 2) {
+            while (getchar() != '\n');
+            printf("!! Invalid input !!\n");
+        } else {
+            dice_choice(player_choice);
+        }
+    } while (player_choice != 3);
+
+    return 0;
+}
