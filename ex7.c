0e3300a01a274ff55df2b70283f0665867500320
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-07 15:20:22 +0300

    should work now

--- ex7.c
+++ ex7.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
 
-//int read_positive() {}
-// bool read_positive(int *value);
+bool read_positive(int *value);
 
 int main(void) {
-    char name[32];
-    printf("Enter your name: ");
-    fgets(name, 32, stdin);
-    if(name[strlen(name) - 1] == '\n') {
-        name[strlen(name) - 1] = '\0';
+    int user_guess = 0;
+    int user_attempts = 0;
+
+    printf("Guess how much money I have!\n");
+
+    do {
+        if (read_positive(&user_guess) != 1) {
+            printf("!! Invalid input !!\n");
+            while (getchar() != '\n');
+            user_attempts += 1;
+        } else {
+            printf("You didn't get it right. I have %d euros.\n", (user_guess * 2) + 20);
+            user_attempts = user_attempts;
+        }
+
+    } while (user_attempts < 3);
+
+    printf("I give up! See you later!\n");
+}
+
+bool read_positive(int *value) {
+
+    printf("Enter a positive number: \n");
+    if (scanf("%d", value) != 1) {
+        return false;
     }
+
+    if (*value > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
