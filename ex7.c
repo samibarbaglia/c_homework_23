704b071768d317d08d94a8945b53b4420284db3e
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-07 15:49:26 +0300

    should work now

--- ex7.c
+++ ex7.c
@@ -11,30 +11,25 @@ int main(void) {
 
     do {
         if (read_positive(&user_guess) != 1) {
-            printf("!! Invalid input !!\n");
-            while (getchar() != '\n');
             user_attempts += 1;
         } else {
             printf("You didn't get it right. I have %d euros.\n", (user_guess * 2) + 20);
             user_attempts = user_attempts;
         }
-
     } while (user_attempts < 3);
 
-    printf("I give up! See you later!\n");
+    printf("\nI give up! See you later!\n");
 }
 
 bool read_positive(int *value) {
 
-    printf("Enter a positive number: \n");
-    if (scanf("%d", value) != 1) {
+    printf("\nEnter a positive number: \n");
+    if ((scanf("%d", value) != 1) || *value < 0) {
+        printf("!! Invalid input !!\n");
+        while (getchar() != '\n');
         return false;
-    }
-
-    if (*value > 0) {
+    } if (*value > 0) {
         return true;
-    } else {
-        return false;
     }
 
 }
