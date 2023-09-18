ad8f42f09fe2d8886f70976cb1289dd378469c2c
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-18 15:20:36 +0300

    ex12

---
+++ ex12.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>so
+
+#define BUFFERMAX 1000
+
+int count_words(const char *str, const char *word) {
+    int count = 0;
+    const char *find_words = str;
+
+    while ((find_words = strstr(find_words, word)) != NULL) {
+        count++;
+        find_words += strlen(word);
+    }
+
+    return count;
+}
+
+int main() {
+    char sentence[BUFFERMAX] = {0};
+    char word[BUFFERMAX] = {0};
+    int count = 0;
+
+    while (strcmp(word, "stop") != 0) {
+        printf("Enter your sentence: \n");
+        fgets(sentence, BUFFERMAX, stdin);
+        sentence[strcspn(sentence, "\n")] = '\0';
+
+        printf("Enter word to search or 'stop' to stop: \n");
+        fgets(word, BUFFERMAX, stdin);
+        word[strcspn(word, "\n")] = '\0';
+
+        if (strcmp(word, "stop") == 0) {
+            printf("Quitting...\n");
+        } else {
+            count = count_words(sentence, word);
+            printf("Appears %d times\n", count);
+        }
+    }
+    return 0;
+}
