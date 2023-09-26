8a9a24252e43832d415d2f6775ccb2bcdac32a41
samibarbaglia <sofia.barbaglia@metropolia.fi>
2023-09-26 23:41:26 +0300

    ex14

--- ex14.c
+++ ex14.c
@@ -2,45 +2,43 @@
 #include <string.h>
 #include <ctype.h>
 
-#define BUFFERMAX 80
+#define MAX_CHARACTERS 81
 #define MAX_LINES 100
 
 int main() {
-    FILE *file_input, *file_output;
-    char filename[BUFFERMAX];
-    char lines[MAX_LINES][BUFFERMAX] = {0};
+    FILE *file;
+    char filename[MAX_CHARACTERS];
+    char lines[MAX_LINES][MAX_CHARACTERS] = {0};
     int lines_read = 0;
 
     printf("Enter file: \n");
-    fgets(filename, BUFFERMAX, stdin);
+    fgets(filename, MAX_CHARACTERS, stdin);
     filename[strcspn(filename, "\n")] = '\0';
-    file_input = fopen(filename, "r");
 
-    if (file_input == NULL) {
+    file = fopen(filename, "r");
+
+    if (file == NULL) {
         fprintf(stderr, "ERROR: No file '%s'\n", filename);
     }
 
-    while (lines_read < MAX_LINES && fgets(lines[lines_read], BUFFERMAX, file_input)) {
+    while (lines_read < MAX_LINES && fgets(lines[lines_read], MAX_CHARACTERS, file)) {
         lines[lines_read][strcspn(lines[lines_read], "\n")] = '\0';
         for (int i = 0; lines[lines_read][i]; i++) {
             lines[lines_read][i] = toupper(lines[lines_read][i]);
         }
         lines_read++;
-    }
-
-    if (lines_read >= MAX_LINES) {
+    } if (lines_read >= MAX_LINES) {
         fprintf(stderr, "WARNING: More lines than allowed. Extra lines deleted.\n");
     }
 
-    fclose(file_input);
-    file_output = fopen(filename, "w");
+    fclose(file);
+    file = fopen(filename, "w");
 
     for (int i = 0; i < lines_read; i++) {
-        fprintf(file_output, "%s\n", lines[i]);
-    }
-    if (lines_read > 0) {
+        fprintf(file, "%s\n", lines[i]);
+    } if (lines_read > 0) {
         printf("Conversion to CapsLock: DONE\n");
     }
 
-    fclose(file_output);
+    fclose(file);
 }
